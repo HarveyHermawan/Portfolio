7bfb51df3c91bc2dfb09639957ab9563b65c28c7
HarveyHermawan <hermawanharvey8@gmail.com>
2025-03-04 14:17:32 +0700

    Add data

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './skills.css'
+import {Skills} from '../../data/skills'
 
 const skills = () => {
   return (
     <div className='skills'>
-      
       <div className='title rounded-3xl'>
         <div className='title-text'>
           Skills
         </div>
+      </div>
+      <div className='content'>
+        {Skills.map((skill) =>(
+          <div className='item' key={skill.id}>
+            <div>
+              <img src={skill.image} alt="" />
+            </div>
+            <div className='skill-name'>{skill.name}</div>  
+          </div>
+        ))}
       </div>
     </div>
   )
---
+++ src/data/skills.js
@@ -0,0 +1,32 @@
+export const skills=[
+    {
+        id: 1,
+        image: '/html.png',
+        name: 'HTML'
+    },
+    {
+        id: 2,
+        image: '/css.png',
+        name: 'CSS'
+    },
+    {
+        id: 3,
+        image: '/js.png',
+        name: 'JavaScript'
+    },
+    {
+        id: 4,
+        image: '/reactjs.png',
+        name: 'React'
+    },
+    {
+        id: 5,
+        image: '/figma.png',
+        name: 'Figma'
+    },
+    {
+        id: 6,
+        image: '/github.png',
+        name: 'Github'
+    }
+]
